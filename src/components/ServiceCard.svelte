0ef1eb1902471593e541ac89f0710742cb8ae6ac
Mao Lee <maoleemao@users.noreply.github.com>
2026-08-23 14:59:19 +0200

    feat: update flight service cards

--- src/components/ServiceCard.svelte
+++ src/components/ServiceCard.svelte
@@ -1,67 +1,71 @@
 <script lang="ts">
-    interface Service {
-        title: string;
-        description: string;
-        imageUrl: string;
-        price: string;
-    }
+	interface Service {
+		title: string;
+		description: string;
+		imageUrl: string;
+		imagePosition?: string;
+		price: string;
+	}
 
-    interface Props {
-        service: Service;
-    }
+	interface Props {
+		service: Service;
+	}
 
-    let { service }: Props = $props();
+	let { service }: Props = $props();
 </script>
 
-<div class="card card-vertical  w-full rounded-lg flex flex-col p-6 gap-4 relative ">
-    <h1 class="text-neutral-950 font-semibold text-lg">{service.title}</h1>
-    
-    <!-- Fixed size container with aspect ratio -->
-    <div class="image-container-vertical rounded-lg object-center">
-        <img src={service.imageUrl} alt={service.title} class="image-vertical object-center">
-    </div>
+<div class="card card-vertical relative flex w-full flex-col gap-4 rounded-lg p-6">
+	<h1 class="text-lg font-semibold text-neutral-950">{service.title}</h1>
 
-    <p class="text-neutral-800">{service.description}</p>
-    <div class="w-full flex row justify-end">
-        <p class="w-fit text-neutral-900 shadow-md text-lg bg-neutral-100/75 text-right py-2 px-4 rounded-lg font-semibold">{service.price}€ - persona</p>
-    </div>
+	<!-- Fixed size container with aspect ratio -->
+	<div class="image-container-vertical rounded-lg object-center">
+		<img
+			src={service.imageUrl}
+			alt={service.title}
+			class="image-vertical object-center"
+			style:object-position={service.imagePosition ?? 'center'}
+		/>
+	</div>
+
+	<p class="text-neutral-800">{service.description}</p>
+	<div class="row flex w-full justify-end">
+		<p
+			class="w-fit rounded-lg bg-neutral-100/75 px-4 py-2 text-right text-lg font-semibold text-neutral-900 shadow-md"
+		>
+			{service.price}€ - persona
+		</p>
+	</div>
 </div>
 
-
-
 <style>
+	.card {
+		background: rgba(242, 242, 242, 0.626);
+		box-shadow: 1px 4px 5px rgba(0, 0, 0, 0.077);
+		transition: all ease-in-out 0.3s;
+	}
 
-    .card {
-        background: rgba(242, 242, 242, 0.626);
-        box-shadow: 1px 4px 5px rgba(0, 0, 0, 0.077);
-        transition: all ease-in-out 0.3s;
-    }
+	.card:hover {
+		background: rgba(242, 242, 242, 0.912);
+	}
 
-    .card:hover {
-        background: rgba(242, 242, 242, 0.912);
-    }
+	/*VERTICAL*/
+	.card-vertical {
+		max-width: 350px;
+	}
 
-    /*VERTICAL*/
-    .card-vertical {
-        max-width: 350px;
-    }
+	/* Image Container to force uniform size */
+	.image-container-vertical {
+		width: 100%;
+		aspect-ratio: 4 / 2.5; /* Ensures consistent height based on width */
+		overflow: hidden;
+		border-radius: 8px;
+	}
 
-    /* Image Container to force uniform size */
-    .image-container-vertical {
-        width: 100%;
-        aspect-ratio: 4 / 2.5; /* Ensures consistent height based on width */
-        overflow: hidden;
-        border-radius: 8px;
-        
-    }
-
-    /* Ensure images cover the entire container */
-    .image-vertical {
-        width: 100%;
-        height: 100%;
-        object-fit: cover; /* Ensures uniform appearance */
-        max-height: 250px;
-    }
-
-
+	/* Ensure images cover the entire container */
+	.image-vertical {
+		width: 100%;
+		height: 100%;
+		object-fit: cover; /* Ensures uniform appearance */
+		max-height: 250px;
+	}
 </style>
--- src/components/sections/ServicesSection.svelte
+++ src/components/sections/ServicesSection.svelte
@@ -1,30 +1,67 @@
 <script>
+	import ServiceCard from '../ServiceCard.svelte';
 
-	import ServiceCard from "../ServiceCard.svelte";
-    
-    const services = [
-        {title: 'Vuelo Biplaza', description:'Disfruta de un vuelo desde diversas zonas del Pirineo. Puede ser un vuelo tranquilo de 10-15 minutos o si las condiciones lo permiten un vuelo mas potente (térmico) de unos 10-30 minutos que no te dejara indiferente.', imageUrl: 'biplaza.webp', price: "150"},
-        {title: 'Hike & Fly', description: 'Actividad que combina trekking guiado de 1-2 horas con el parapente a la espalda y salir de un sitio poco accesible y privilegiado de vuelo. Para aquellos que prefieren ganarse el descenso. Vuelo de 15-30 minutos (depende condiciones).', imageUrl: 'hike-fly.webp', price: "250"}
-    ]
-
+	const services = [
+		{
+			title: 'Vuelo Valle',
+			description:
+				"El vuelo perfecto para conocer la Val d'Aran. Combina 40 minutos de trekking con el parapente a la espalda y 15 minutos de vuelo. Despegando de un sitio poco accesible y privilegiado de vuelo. Con vistas al Parc Nacional d'Aigüestortes y al glaciar del Aneto.",
+			imageUrl: 'vuelo-valle.webp',
+			imagePosition: 'center 46%',
+			price: '200'
+		},
+		{
+			title: 'Vuelo térmico',
+			description:
+				"Disfruta de un vuelo más largo donde ganamos altura gracias a las corrientes térmicas. Vuelo de al menos 30 minutos, con posibilidad de alargar siempre que la meteorología lo permita. Este vuelo también se puede combinar con un trekking de 40'.",
+			imageUrl: 'vuelo-termico.webp',
+			imagePosition: 'center 34%',
+			price: '250'
+		},
+		{
+			title: 'Vuelo Luchon',
+			description:
+				'Si eres alguien que únicamente quiere volar y no quieres andar más de 5 minutos, este vuelo es para ti. Un despegue impresionante en la estación de Superbagnères de Luchon. Vuelo de 15 minutos de duración.',
+			imageUrl: 'hike-fly.webp',
+			imagePosition: 'center center',
+			price: '160'
+		},
+		{
+			title: 'Vuelo con Buggie',
+			description:
+				'En este vuelo empezamos a tope de principio a fin. Sube en Buggie hasta la antigua estación de la Tuca. Haz una pequeña excursión de 20 minutos y disfruta del único vuelo donde sobrevolamos Vielha, unas vistas únicas e irrepetibles.',
+			imageUrl: 'vuelo-buggie.webp',
+			imagePosition: 'center 48%',
+			price: '230'
+		}
+	];
 </script>
-<section id="services-section" class="h-full w-full flex flex-col justify-center items-center gap-10 px-4 lg:px-18 py-16 lg:py-28">
-        <div class="flex flex-col gap-2 w-full">
-            <h1 class="sifon text-center text-3xl lg:text-4xl text-neutral-900">
-               DESCUBRE NUESTROS VUELOS <br>
-            <span class="text-[2rem] lg:text-[2.5rem]">MAS ESPECIALES</span></h1>
-            <p class="font-sans text-md lg:text-xl text-center text-neutral-900 pt-4">Vuelos tranquilos de tarde, vuelos térmicos de adrenalina o actividad combinada de Trekking y Vuelo desde puntos escondidos.</p>
-        </div>
-        <div class="services w-full flex flex-col md:flex-row gap-6 lg:gap-8 justify-center items-center">
-            {#each services as service}
-            <ServiceCard {service}/>
-            {/each}
-        </div>
-        
+
+<section
+	id="services-section"
+	class="flex h-full w-full flex-col items-center justify-center gap-10 px-4 py-16 lg:px-18 lg:py-28"
+>
+	<div class="flex w-full flex-col gap-2">
+		<h1 class="sifon text-center text-3xl text-neutral-900 lg:text-4xl">
+			DESCUBRE NUESTROS VUELOS <br />
+			<span class="text-[2rem] lg:text-[2.5rem]">MAS ESPECIALES</span>
+		</h1>
+		<p class="text-md pt-4 text-center font-sans text-neutral-900 lg:text-xl">
+			Vuelos tranquilos de tarde, vuelos térmicos de adrenalina o actividad combinada de Trekking y
+			Vuelo desde puntos escondidos.
+		</p>
+	</div>
+	<div
+		class="services grid w-full max-w-7xl grid-cols-1 place-items-center gap-6 md:grid-cols-2 xl:grid-cols-4"
+	>
+		{#each services as service}
+			<ServiceCard {service} />
+		{/each}
+	</div>
 </section>
 
 <style>
-   section{
-    background: var(--primaryGradientVertical); 
-   }
+	section {
+		background: var(--primaryGradientVertical);
+	}
 </style>
